3457dc29d92fd4c6c13f07d9f0bc13270a79f5bd
MarcelaBazela <116001582+MarcelaBazela@users.noreply.github.com>
2023-11-28 15:46:58 +0100

    header with new div tag names and search bar added

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
-var checkbox = document.getElementById("check");
+var checkbox = document.getElementById("nav-toggle");
 
 checkbox.addEventListener("change", function () {
   document.body.classList.toggle("no-scroll", checkbox.checked);
